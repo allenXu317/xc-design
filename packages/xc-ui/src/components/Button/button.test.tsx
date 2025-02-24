e642f87d803d571825129de793b09961af0a4409
AllenHsu <xuchen_allen@163.com>
2025-02-25 07:42:39 +0800

    feat:集成jest-dom&完善测试用例

--- packages/xc-ui/src/components/Button/button.test.tsx
+++ packages/xc-ui/src/components/Button/button.test.tsx
@@ -1,9 +1,32 @@
 import { render } from '@testing-library/react';
 import { Button } from './button';
+import '@testing-library/jest-dom';
+import { ButtonSize, ButtonType } from './constants';
 
 test('our first react test case', () => {
   const wrapper = render(<Button>Nice</Button>);
   const element = wrapper.queryByText('Nice');
-  console.log(element);
   expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
+});
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button>Nice</Button>);
+    const element = wrapper.getByText('Nice');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('BUTTON');
+    expect(element).toHaveClass('btn btn-default');
+  });
+
+  it('should render the correct component based on different props', () => {
+    const wrapper = render(
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large} className="klass">
+        Nice
+      </Button>
+    );
+    const element = wrapper.getByText('Nice');
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass('btn-primary btn-lg klass');
+  });
 });
